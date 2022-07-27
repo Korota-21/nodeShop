7bc8bfd5792c55141312fce419b339b2c1fcf75e
Marwa <koroko123430@gmail.com>
2022-07-27 08:49:51 +0300

    query

--- controllers/product.js
+++ controllers/product.js
@@ -1,12 +1,24 @@
 const Product = require("../models/product")
 const validationHandler = require("../validations/validationHandler")
 
-exports.index = async(req, res) => {
+exports.index = async(req, res, next) => {
     try {
         const pagination = req.query.pagination ? parseInt(req.query.pagination) : 50;
         const page = req.query.page ? parseInt(req.query.page) : 1;
+        const tags = req.query.tags
+        const colors = req.query.colors
+        const availability = req.query.availability ? JSON.parse((req.query.availability).toLowerCase()) : null;
+        let query = {}
+        if (!(availability == null))
+            query.availability = availability;
+        if (!(tags == null))
+            query.tags = tags;
+        if (!(colors == null))
+            query.colors = colors;
 
-        const products = await Product.find().skip((page - 1) * pagination).limit(pagination).sort({ createdAt: -1 });
+        const products = await Product.find(query)
+            .skip((page - 1) * pagination).limit(pagination)
+            .sort({ createdAt: -1 });
         res.send(products);
     } catch (err) {
         next(err);
@@ -43,7 +55,10 @@ exports.store = async(req, res, next) => {
         product.tags = req.body.tags;
         product.colors = req.body.colors;
         product.description = req.body.description;
-        product.availability = req.body.availability;
+        if (product.quantity > 0)
+            product.availability = req.body.availability;
+        else
+            product.availability = false;
         product = await product.save()
         res.send(product);
     } catch (err) {
@@ -69,7 +84,10 @@ exports.update = async(req, res, next) => {
         product.quantity = req.body.quantity;
         product.tags = req.body.tags;
         product.colors = req.body.colors;
-        product.availability = req.body.availability;
+        if (product.quantity > 0)
+            product.availability = req.body.availability;
+        else
+            product.availability = false;
         product.description = req.body.description;
         product = await product.save()
         res.send(product);
